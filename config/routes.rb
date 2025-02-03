2d8972543c16c651bd9cee1422bb64e0a798d3e8
Muhammad Abdullah <mabdullah5514@yahoo.com>
2025-02-03 21:20:01 +0500

    Added routes and schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
+# config/routes.rb
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      # Admin endpoints
+      resources :projects, only: [:index] do
+        post 'assign', on: :member
+        delete 'unassign', on: :member
+        get 'task_breakdown', on: :member
+      end
+
+      # User endpoints
+      resources :tasks, only: [:create]
+      get 'my_projects', to: 'projects#assigned_projects'
+    end
+  end
 end
